0735de177582d7fafefa8bf6eb74f300ca0cb027
MoRevolution <morevolution12@gmail.com>
2023-05-10 14:19:48 -0500

    First Commit and First Leg Completed

---
+++ ecommerce/ecommerce/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+from core.views import frontpage, about
+
+urlpatterns = [
+    path('about/', about, name='about'),
+    path('admin/', admin.site.urls),
+    path('', include('userprofile.urls')), # should always be matched before store.urls
+    path('', include('store.urls')),
+    path('', frontpage, name='frontpage'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
