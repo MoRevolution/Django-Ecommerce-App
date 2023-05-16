1a964b0b2a9c99e210290057c70b9c55063d1246
MoRevolution <morevolution12@gmail.com>
2023-05-15 19:26:08 -0500

    Basic Login Page and Signup Page

--- ecommerce/userprofile/urls.py
+++ ecommerce/userprofile/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('myaccount/', views.myaccount, name='myaccount'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('vendors/<int:pk>/', views.vendor_detail, name='vendor_detail'),
-
 ]
--- ecommerce/userprofile/views.py
+++ ecommerce/userprofile/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .models import Userprofile
+
 
 # Create your views here.
 def vendor_detail(request, pk):
@@ -8,3 +12,27 @@ def vendor_detail(request, pk):
     return render(request, 'userprofile/vendor_detail.html', {
         'user': user,
     })
+
+def myaccount(request): 
+    return render(request, 'userprofile/myaccount.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            userprofie = Userprofile.objects.create(user=user)
+
+            return redirect('frontpage')
+    else: 
+        form = UserCreationForm()
+
+    return render(request, 'userprofile/signup.html', {
+        'form': form,
+    })
+
+
